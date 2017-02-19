c3dc20a347b6f6d7826cd0bdc71ae341aac6910e
Jack Shultz <jackygrahamez@gmail.com>
2017-02-19 14:05:32 -0500

    removed title and description assignment

--- src/routes/index.js
+++ src/routes/index.js
@@ -32,11 +32,6 @@ export default {
   async action({ next }) {
     // Execute each child route until one of them return the result
     const route = await next();
-
-    // Provide default values for title, description etc.
-    route.title = `${route.title || 'Untitled Page'} - www.reactstarterkit.com`;
-    route.description = route.description || '';
-
     return route;
   },
 
